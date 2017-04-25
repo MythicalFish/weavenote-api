3f36700a263ffa104f5f6519395c8c32d3acdc90
jake <jake@vertaxe.com>
2017-04-25 19:10:55 +0200

    add project attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,15 @@
 class Project < ApplicationRecord
+
   belongs_to :user
+  belongs_to :development_stage
   has_many :materials, through: :users
   has_many :images, as: :imageable
-  has_one :development_stage
   accepts_nested_attributes_for :images
+
+  alias_attribute :stage, :development_stage
+
+  def thumbnail_url
+    images.first.url
+  end
+
 end

42a60bb2a6c4313503f22371c800b97fafbcb5ed
jake <jake@vertaxe.com>
2017-05-19 18:52:17 +0200

    add & modify models

--- app/models/material.rb
+++ app/models/material.rb
@@ -5,4 +5,14 @@ class Material < ApplicationRecord
 
   alias_attribute :type, :material_type
 
+  before_validation :set_currency
+
+  private
+
+  def set_currency
+    unless self.currency_id
+      self.currency_id = Currency.find_by_iso_code('GBP')
+    end
+  end
+
 end

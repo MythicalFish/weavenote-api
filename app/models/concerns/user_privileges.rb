3c0579a464302f4c56372774f719c09494a1e993
jake <jake@vertaxe.com>
2017-06-07 21:57:15 +0200

    add privileges

--- app/models/concerns/user_privileges.rb
+++ app/models/concerns/user_privileges.rb
@@ -2,5 +2,38 @@ module UserPrivileges
   extend ActiveSupport::Concern
   included do
 
+    def can? action, object
+      if object.model_name.name === 'Project'
+        role = self.project_role(object)
+        return false unless role
+        return priv_map[role.type.name][action]
+      end
+      false
+    end
+
   end
+
+  def priv_map
+    {
+      'None' =>    actions(0,0,0,0),
+      'Viewer' =>  actions(0,1,0,0),
+      'Editor' =>  actions(0,1,1,0),
+      'Manager' => actions(1,1,1,1),
+      'Admin' =>   actions(1,1,1,1)
+    }
+  end
+
+  def bool i
+    i > 0
+  end
+
+  def actions c, r, u, d
+    {
+      create: bool(c),
+      read:   bool(r),
+      update: bool(u),
+      delete: bool(d),
+    }
+  end
+
 end

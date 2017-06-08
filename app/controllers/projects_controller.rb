67a02b74e5496ceefdc50c36d286ac2854e8d43e
jake <jake@vertaxe.com>
2017-06-08 13:29:33 +0200

    add material_cost

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
   def show
     render json: {
       project: @project,
+      material_cost: @project.material_cost,
       role: @user.project_role(@project).type.attributes
     }
   end

f25cf2cc8cff4fc08f2788878100a645942b57c4
jake <jake@vertaxe.com>
2017-07-05 11:48:26 +0200

    update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :avatar, :abilities
-  belongs_to :organization
-  has_many :organizations
+  attributes :id, :name, :email, :avatar, :abilities, :organization, :organizations, :organization_role_type
 end

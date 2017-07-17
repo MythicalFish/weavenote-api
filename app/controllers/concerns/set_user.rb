17134a45e5a0bcfeb56c4c98b83fb91ffab07528
jake <jake@vertaxe.com>
2017-07-17 17:24:10 +0200

    refactor set_user

--- app/controllers/concerns/set_user.rb
+++ app/controllers/concerns/set_user.rb
@@ -7,33 +7,35 @@ module SetUser
   CACHE = ActiveSupport::Cache::MemoryStore.new(expires_in: 30.minutes) # this caches in Development too
 
   def set_user!
-    @user = User.find_by_auth0_id(auth0_id)
-    unless @user
-      @user = User.create({
-        name: user_info['nickname'],
-        email: user_info['email'],
-        avatar: user_info['picture'],
-        auth0_id: auth0_id
-      })
-    end
+    @user = find_user
+    @user = create_user unless @user
     @organization = @user.organization
   end
 
-  def auth0_id
-    CACHE.fetch(cache_key_for_auth0_id) do
-      user_info['user_id']
-    end
+  def find_user
+    User.find_by_auth0_id(user_info['user_id'])
+  end
+
+  def create_user
+    User.create!({
+      name: user_info['nickname'],
+      email: user_info['email'],
+      avatar: user_info['picture'],
+      auth0_id: user_info['user_id']
+    })
   end
 
   def user_info
-    secrets = Rails.application.secrets
-    auth0 = Auth0Client.new(
-      :client_id => secrets.auth0_client_id,
-      :token => token,
-      :domain => secrets.auth0_domain,
-      :api_version => 2
-    )
-    auth0.user_info
+    CACHE.fetch(cache_key_for_token) do
+      secrets = Rails.application.secrets
+      auth0 = Auth0Client.new(
+        :client_id => secrets.auth0_client_id,
+        :token => token,
+        :domain => secrets.auth0_domain,
+        :api_version => 2
+      )
+      auth0.user_info
+    end
   end
 
   def token
@@ -43,8 +45,8 @@ module SetUser
     token
   end
 
-  def cache_key_for_auth0_id
-    "auth0_id_from_token___#{token}"
+  def cache_key_for_token
+    "user_info_from_token___#{token}"
   end
 
 end

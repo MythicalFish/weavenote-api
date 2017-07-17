f9f60b6e993ff692feb97b9e482c22c9e672ecd0
jake <jake@vertaxe.com>
2017-07-17 12:47:13 +0200

    improve roles controller

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -5,21 +5,25 @@ class RolesController < ApplicationController
   before_action :set_role, only: [:update, :destroy]
 
   def index
-    render json: @invitable.roles.where(role_type_id:RoleType::EXPOSED_IDS)
+    render json: roles
   end
 
   def update
     @role.update!(role_params)
-    index
+    render_success "Role updated", roles
   end
   
   def destroy
     @role.destroy!
     @collaborator.update!(current_organization_id: 0) if @invitable.class.name == 'Organization'
-    render_success "Collaborator removed from #{@invitable.class.name}", @invitable.roles
+    render_success "Collaborator removed from #{@invitable.class.name}", roles
   end
 
   private
+
+  def roles
+    @invitable.roles.where(role_type_id:RoleType::EXPOSED_IDS)
+  end
 
   def set_role
     @role = @invitable.roles.find(params[:id])

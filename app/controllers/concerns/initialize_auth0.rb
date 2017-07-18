391deaf0a1b893c0008a8ab30b20a0bd50914f42
jake <jake@vertaxe.com>
2017-07-18 14:29:40 +0200

    use auth0 gem for reset_password

--- app/controllers/concerns/initialize_auth0.rb
+++ app/controllers/concerns/initialize_auth0.rb
@@ -5,7 +5,11 @@ module InitializeAuth0
   AUTH0_CACHE = ActiveSupport::Cache::MemoryStore.new(expires_in: 30.minutes)
 
   def initialize_auth0!
-    @auth0 = AUTH0_CACHE.fetch(cache_key_for_token) do
+    @auth0 = auth0_client
+  end
+  
+  def auth0_client 
+    AUTH0_CACHE.fetch(cache_key_for_token) do
       secrets = Rails.application.secrets
       Auth0Client.new(
         :client_id => secrets.auth0_client_id,
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
   
-  require 'uri'
-  require 'net/http'
-  
   def show
     render json: @user
   end
-
-  def self.show() show end
 
   def update
     @user.update!(user_params)
@@ -15,17 +10,9 @@ class UsersController < ApplicationController
   end
 
   def reset_password
-    secrets = Rails.application.secrets
-    url = URI("https://#{secrets.auth0_domain}/dbconnections/change_password")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(url)
-    request["content-type"] = 'application/json'
-    request.body = "{\"client_id\": \"#{secrets.auth0_client_id}\",\"email\": \"#{@user.email}\",\"connection\": \"Username-Password-Authentication\"}"
-    response = http.request(request)
-    if response.code == "200"
-      render_success "Please check your email to reset your password"
+    p = @auth0.change_password @user.email, nil
+    if p
+      render_success p
     else
       render_fatal "Something went wrong, unable to reset password"
     end

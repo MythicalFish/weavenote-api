d289ccff738ee7f47525ee257c8bdc172b39fe6b
jake <jake@vertaxe.com>
2017-07-18 21:55:35 +0200

    serialize comment replies

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,10 @@ class Comment < ApplicationRecord
   
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  has_many :replies, -> (o) {
-      where('commentable_id = ? AND commentable_type = ?', o.id, 'Comment')
-    }, class_name: 'Comment'
   has_many :images, as: :imageable  
   delegate :organization, to: :user
+  has_many :comments, as: :commentable
+  alias_attribute :replies, :comments
 
   validates :text, length: { minimum: 2 }
 
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :text, :created_at, :updated_at
   belongs_to :user, serializer: CollaboratorSerializer
+  has_many :replies, serializer: CommentSerializer
 end

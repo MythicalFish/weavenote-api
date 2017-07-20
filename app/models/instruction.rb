0c3bab2bb1f9dcf98be49172aee989bfe1e96879
jake <jake@vertaxe.com>
2017-07-20 15:55:24 +0200

    don't enforce single image

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,14 +1,8 @@
 class Instruction < ApplicationRecord
 
   belongs_to :project
-  has_many :images, as: :imageable, before_add: :enforce_single_image
+  has_many :images, as: :imageable
 
   validates :title, length: { minimum: 3 }
-
-  private
-
-  def enforce_single_image image
-    images.destroy_all
-  end
 
 end

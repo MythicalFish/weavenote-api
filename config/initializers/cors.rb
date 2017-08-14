6942a224793fcd051b043f2d969a480068252ea2
jake <jake@vertaxe.com>
2017-08-14 11:49:20 +0200

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://seamless.mythical.fish', 'https://weavenote.firebaseapp.com/'
+    origins 'http://localhost:3000', 'https://seamless.mythical.fish', 'https://weavenote.firebaseapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]

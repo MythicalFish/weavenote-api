16d049bd4436f7d31fdda68b3d70a95ce3bbafe0
jake <jake@vertaxe.com>
2017-08-22 11:25:40 +0200

    headless

--- app/models/spec_sheet.rb
+++ app/models/spec_sheet.rb
@@ -1,3 +1,13 @@
+class PDFKitHeadless(PDFKit)
+  def command(self, path=None)
+    if Rails.env.development?
+      return ['xvfb-run', '--'] + super().command(path)
+    else
+      return super().command(path)
+    end
+  end
+end
+
 class SpecSheet
 
   def initialize(filename, project)
@@ -13,7 +23,7 @@ class SpecSheet
 
   def create_pdf
     filepath = "#{Rails.root}/tmp/#{@filename}"
-    kit = PDFKit.new(html, self.config)
+    kit = PDFKitHeadless(PDFKit).new(html, self.config)
     kit.stylesheets << "#{Rails.root}/lib/pdfkit/style.css"
     kit.to_file(filepath)
   end

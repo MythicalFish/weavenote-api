f8bc0011d258f9131728eb72792b4150234e7765
jake <jake@vertaxe.com>
2017-10-27 17:33:25 +0200

    export annotations

--- app/models/annotation_anchor.rb
+++ app/models/annotation_anchor.rb
@@ -1,3 +1,12 @@
 class AnnotationAnchor < ApplicationRecord
   belongs_to :annotation, required: false
+
+  def x_percent
+    "#{x * 100}%"
+  end
+  
+  def y_percent
+    "#{y * 100}%"
+  end
+  
 end

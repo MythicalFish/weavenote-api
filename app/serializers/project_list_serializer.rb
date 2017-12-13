72b96c5b5b7f0677ac46b0b6df91df00569c0852
jake <jake@mythical.fish>
2017-12-13 19:31:40 +0100

    color code

--- app/serializers/project_list_serializer.rb
+++ app/serializers/project_list_serializer.rb
@@ -1,4 +1,4 @@
 class ProjectListSerializer < ActiveModel::Serializer
-  attributes :id, :name, :ref_number, :stage, :archived, 
+  attributes :id, :name, :ref_number, :stage, :archived, :color_code, 
   :all_collaborators, :collection, :thumbnail_url, :created_at
 end
